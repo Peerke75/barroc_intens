8a11d43d4d893aa8847a2976be0bfadcc08d6188
Lululink1 <91091619+Lululink1@users.noreply.github.com>
2024-11-08 11:52:46 +0100

    Models aangepast
    Ik heb de models overzichtelijker gemaakt

--- app/Models/CustomerService.php
+++ app/Models/CustomerService.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerService extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'customer_services';
 }
--- app/Models/Malfunction.php
+++ app/Models/Malfunction.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Malfunction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'malfunctions';
 }
