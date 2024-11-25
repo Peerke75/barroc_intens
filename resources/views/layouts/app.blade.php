edb2dce9cd4c25c0f5bac065968c1fa8be1cf90b
Peerke75 <d297354@edu.curio.nl>
2024-11-25 09:39:18 +0100

    leaflet gemaakt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">   
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <header>
   @include('components.nav-menu')
@@ -12,6 +13,7 @@
 
 <body>
   @yield('content')
+  <script src="script.js"></script>
 
 </body>
 <footer>
