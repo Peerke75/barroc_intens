159da35ee95ad053052896053779b8c685c4ecab
Mnighter <malik11112222333344445555@gmail.com>
2025-01-08 17:00:31 +0100

    start_appointment vergeten bij te voegen  bij migrations sales
    Ik heb de start_appointment erbij gevoegd want die werd niet in de migration sales aangeroepen maar wel in de seeder. heb ik gemerkt wanneer ik migrate fresh probeert te doen maar nu is die probleem opgelost

--- database/migrations/2024_11_29_080300_create_sales_table.php
+++ database/migrations/2024_11_29_080300_create_sales_table.php
@@ -21,7 +21,8 @@ return new class extends Migration
         $table->string('location');
         $table->date('date');
         $table->string('status');
-        $table->dateTime('end_appointment');
+        $table->dateTime('start_appointment')->nullable();
+        $table->dateTime('end_appointment')->nullable();
         $table->timestamps();
         });
     }
